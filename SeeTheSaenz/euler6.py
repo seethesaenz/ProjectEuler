5e2c260eca3079b429e1ba3941860917cd7ff836
seethesaenz <60051487+seethesaenz@users.noreply.github.com>
2020-01-30 04:46:27 -0600

    added notes
    added notes and made code more readable

--- SeeTheSaenz/euler6.py
+++ SeeTheSaenz/euler6.py
@@ -1,16 +1,18 @@
-#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the 
+# sum of the first 100 numbers. 
 def sumSquare():
     list1 = []
     x = 1
+    # made list of 1-100
     for i in range(100):
         list1.append(x)
         x += 1
+    # made list with 1-100 squared
     squares = []
     for number in list1:
         squares.append(pow(number, 2))
     sumOfSquares = sum(squares)
+    # squaring the sum of the list of 1-100
     squareOfSum = pow(sum(list1), 2)
-    print(squareOfSum-sumOfSquares)
-
-
+    print(squareOfSum - sumOfSquares)
 sumSquare()
