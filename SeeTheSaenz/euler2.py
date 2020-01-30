3a3137290d458de3b11e8aa1e2a0ff9207766808
seethesaenz <60051487+seethesaenz@users.noreply.github.com>
2020-01-30 04:24:56 -0600

    added notes
    added notes and made code easier to read

--- SeeTheSaenz/euler2.py
+++ SeeTheSaenz/euler2.py
@@ -1,18 +1,28 @@
-#By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 fibonacci = [1, 2]
 evenFibonacci = []
-x=0
+x = 0
+
+
+# making list of fibbonacci including both even and odd
 def fibbonacci(x):
+    # setting the limits of the sequence to not exceed 4M
     while fibonacci[-1] <= 4000000:
-        fibonacci.append(fibonacci[x]+fibonacci[x+1])
-        x = x+1
-fibbonacci(x)
-fibonacci.pop(-1)
-print(fibonacci)
+        # adding on the next sequence and counting up after
+        fibonacci.append(fibonacci[x] + fibonacci[x + 1])
+        x = x + 1
+    # added this because the limit we set was exceeded so correct our list. there's probably a more elegent way but this works
+    fibonacci.pop(-1)
+    evenodd()
+
+
+# decide to just take the values that are even and appending to new list. Don't know if its more efficient to just
+# fibonacci.remove() the odd values instead
 def evenodd():
     for i in fibonacci:
         if i % 2 == 0:
             evenFibonacci.append(i)
-evenodd()
-print(evenFibonacci)
+
+
+fibbonacci(x)
 print(sum(evenFibonacci))
