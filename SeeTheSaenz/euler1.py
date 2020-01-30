334dfabef6a21d0515417f7d045f0092e352f176
seethesaenz <60051487+seethesaenz@users.noreply.github.com>
2020-01-30 04:13:49 -0600

    added in notes to explain code
    fixed code to be more readable

--- SeeTheSaenz/euler1.py
+++ SeeTheSaenz/euler1.py
@@ -1,23 +1,17 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
-
-list1 = [0]
-i =0
-def unique(list1):
-    list_set = set(list1)
-    unique_list = (list(list_set))
-    for x in unique_list:
-        listUni.append(x)
-while i <= 997:
-    list1.append(i+3)
-    i = i+3
-i=0
-while i <= 990:
-    list1.append(i+5)
-    i = i+5
-list1.sort()
-listUni=[]
-
-print(list1)
-unique(list1)
-print(listUni)
-print(sum(listUni))
+def some_multiples():
+    list1 = [0]
+    #making list of all the multiples of 3
+    i=0
+    while i <= 997:
+        list1.append(i+3)
+        i = i+3
+    i=0
+    #adding in the multiples of 5 to the list
+    while i <= 990:
+        list1.append(i+5)
+        i = i+5
+    #this is using set() to get rid of multiple numbers in list e.g. 15 is a multiple of both 3 and 5 so it shows up in list twice
+    list2=list(set(list1))
+    return sum(list2)
+print(some_multiples())
