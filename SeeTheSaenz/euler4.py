29698a1d404e9780aabcd0627e4230d645c463f7
seethesaenz <60051487+seethesaenz@users.noreply.github.com>
2020-01-30 04:34:19 -0600

    added in notes
    added in notes and made code more readable

--- SeeTheSaenz/euler4.py
+++ SeeTheSaenz/euler4.py
@@ -1,22 +1,29 @@
-#Find the largest palindrome made from the product of two 3-digit numbers.
+# Find the largest palindrome made from the product of two 3-digit numbers.
 def palindrome():
     biggestPalin = [0]
+    # setting the upper limit
     x = 1000
     y = 1000
-    while(y > 100):
+    # setting the lower limits
+    while y > 100:
 
-        while (x > 100):
-
+        while x > 100:
+            # brute forcing my way through every product starting at 999*999 and working down to 100*100
             maybePalin = x * y
+            # converting the product into a list
             maybe1Palin = [int(i) for i in str(maybePalin)]
+            # making new list the save as first list but reversed
             maybe2Palin = maybe1Palin[::-1]
-            if(maybe1Palin == maybe2Palin):
-                if(len(maybe1Palin)>len(biggestPalin) or maybe1Palin[0]>biggestPalin[0]):
+            # comparing both lists if they are the same aka palindromic(is that a word lol)
+            if (maybe1Palin == maybe2Palin):
+                # setting the biggest palindrome if the product is longer or starts with a bigger number could be
+                # broken if I had for example a palindrome 960069 and 980089. currently would not register that the
+                # latter is in fact bigger
+                if len(maybe1Palin) > len(biggestPalin) or maybe1Palin[0] > biggestPalin[0]:
                     biggestPalin = maybe1Palin
             x -= 1
         x = 1000
         y -= 1
         print(biggestPalin)
+
 palindrome()
-
-
