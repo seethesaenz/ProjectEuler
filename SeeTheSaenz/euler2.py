dff14b94ca6448edd03614bbe924ad26de9e865e
seethesaenz <60051487+seethesaenz@users.noreply.github.com>
2020-01-30 03:48:59 -0600

    Add files via upload

---
+++ SeeTheSaenz/euler2.py
@@ -0,0 +1,17 @@
+fibonacci = [1, 2]
+evenFibonacci = []
+x=0
+def fibbonacci(x):
+    while fibonacci[-1] <= 4000000:
+        fibonacci.append(fibonacci[x]+fibonacci[x+1])
+        x = x+1
+fibbonacci(x)
+fibonacci.pop(-1)
+print(fibonacci)
+def evenodd():
+    for i in fibonacci:
+        if i % 2 == 0:
+            evenFibonacci.append(i)
+evenodd()
+print(evenFibonacci)
+print(sum(evenFibonacci))
